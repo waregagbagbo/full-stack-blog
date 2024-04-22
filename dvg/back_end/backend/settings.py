98fcc5c23f2fe5e7cd13403930809a0b14b877d8
waregagbagbo <gabelwarega@gmail.com>
2024-04-22 13:03:55 +0300

    installed vue and its dependancies

--- dvg/back_end/backend/settings.py
+++ dvg/back_end/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'graphene_django',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 GRAPHENE = {
     "SCHEMA": "blog.schema.schema",
 }
+
+# corsheaders settings
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",) # specific to localhost frontend
