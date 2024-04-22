9db25995d1a803d2af28e1b82d6a472bb8038a81
waregagbagbo <gabelwarega@gmail.com>
2024-04-22 11:47:38 +0300

    initialized graphene and core settings

--- dvg/back_end/backend/settings.py
+++ dvg/back_end/backend/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
-    'graphene-django',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Graphene configs
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema",
+}
